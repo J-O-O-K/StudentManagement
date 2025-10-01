f56c28f33bc1d80cdb39b323b602db26aee14883
JJ <jaju42190@edu.ucl.dk>
2025-10-01 03:52:36 +0200

    rename Grade. EF made a non-destructive migration. Destructive would've been fine as well in this case since there's no data.

--- StudentManagement/Domain/Models/Enrollment.cs
+++ StudentManagement/Domain/Models/Enrollment.cs
@@ -11,8 +11,7 @@ namespace StudentManagement.Domain.Models
         public int Id { get; set; }
         public int StudentId { get; set; }
         public int CourseId { get; set; }
-        public string? Grade { get; set; } 
-
+        public string? FinalGrade { get; set; } 
         public Student? Student { get; set; }
         public Course? Course { get; set; }
     }
---
+++ StudentManagement/Migrations/Sql/V5__RenameGrade.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+EXEC sp_rename N'[Enrollments].[Grade]', N'FinalGrade', 'COLUMN';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251001015049_RenameGradeToFinalGrade', N'9.0.9');
+
+COMMIT;
+GO
+
