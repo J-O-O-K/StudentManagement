ae5772d555722d0c955843c50e222102f321f259
JJ <jaju42190@edu.ucl.dk>
2025-10-02 00:01:00 +0200

    add MiddleName to Student class

--- StudentManagement/Domain/Models/Student.cs
+++ StudentManagement/Domain/Models/Student.cs
@@ -10,6 +10,7 @@ namespace StudentManagement.Domain.Models
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
+        public string MiddleName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public DateTime EnrollmentDate { get; set; }
